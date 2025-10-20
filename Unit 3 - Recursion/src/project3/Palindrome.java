f9cf91e7812a9d9f024738569d303def596ae349
tommatosPC <carlostommatos@gmail.com>
2025-10-19 21:36:43 -0400

    Fixed palindrome

--- Unit 3 - Recursion/src/project3/Palindrome.java
+++ Unit 3 - Recursion/src/project3/Palindrome.java
@@ -13,93 +13,27 @@ public class Palindrome {
     }
 
 
-
-
     public static boolean isPalindrome(String word) {
 
+        // clean up the string by removing spaces
+        // and setting all letters to lower case
         word = word.toLowerCase();
         word = word.replace(" ", "");
-        char [] word2 = word.toCharArray();
-        String prod;
-        boolean tf = true;
 
-        if (word2 [0] != word2 [word2.length - 1]) {
-            System.out.println("I'm here");
-            tf = false;
+        // if any of the letters are not the same, the word is not a palindrome
+        if (word.charAt(0) != word.charAt(word.length() - 1))
+            return false;
 
-        }
+        // if the middle two letters are the same (0 letters left)
+        // or there is only one letter left, the word is a palindrome
+        else if (word.length() <2)
+            return true;
 
+        // move on to the next letter of the word
+        String shortenedWord = word.substring(1, word.length() - 1);
+        return isPalindrome(shortenedWord);
 
-        else {
-
-            word2 [0] = ' ';
-            word2 [word2.length - 1] = ' ';
-            prod = new String(word2);
-            prod = prod.replace(" ", "");
-            prod = prod.trim();
-            System.out.println(prod);
-            isPalindrome(prod);
-
-        }
-
-        return tf;
     }
-
-
-
-
-
-    // make a variable for wordlength and another for what should change
-//    public static boolean isPalindrome(String word, int n) {
-//
-//        word = word.replace(" ", "");
-//        word = word.toLowerCase();
-//        System.out.println("n: " + n);
-//        if (word.length() - n < word.length() / 2)
-//            return true;
-//
-//        System.out.println(word.charAt(word.length() - 1 - n));
-//        System.out.println(word.charAt(n));
-//
-//        if (word.charAt(word.length() - n - 1) == word.charAt(n))
-//            return isPalindrome(word, n + 1);
-//
-//        return false;
-//
-//
-//
-//    }
-
-
-
-
-
-//    public static boolean isPalindrome(String word1, int n) {
-//
-//
-//        String word2 = word1.replace(" ", ""); // removes spaces from the word
-//        word2 = word2.toLowerCase(); // makes all of the letters lowercase
-//
-//
-//        // base case: if every letter past the halfway point is the same,
-//        // the word is a palindrome
-//        if (n <= word2.length() / 2) {
-//
-//            return true;
-//
-//        }
-//
-//        if (word2.charAt(n - 1) == word2.charAt(word2.length() - n)) {
-//
-//            return isPalindrome(word2, n - 1);
-//
-//        }
-//
-//
-//
-//        return false;
-//
-//    }
 
 
 
